c8ab348cc93b4948c269785cceab2cf4fc814907
Shohei Kikuchi <killchance2256@gmail.com>
2022-05-06 19:50:47 +0900

    reduxtoolkit tutorials

---
+++ src/features/counter/counter.tsx
@@ -0,0 +1,28 @@
+import { RootState } from '../../app/store'
+import { useAppSelector, useAppDispatch } from '../../app/hooks'
+import { decrement, increment } from './counterSlice'
+
+export function Counter() {
+  const count = useAppSelector((state: RootState) => state.counter.value)
+  const dispatch = useAppDispatch()
+
+  return (
+    <div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  )
+}
